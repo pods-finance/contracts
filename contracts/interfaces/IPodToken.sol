9ed0228828f9b3e45a654d14734675ae426fbd1c
Robsonsjre <robsonsjre@gmail.com>
2020-05-24 15:40:26 -0300

    added podToken interface

--- contracts/interfaces/IPodToken.sol
+++ contracts/interfaces/IPodToken.sol
@@ -8,19 +8,33 @@ interface IPodToken {
 
     function withdraw(address) external;
 
+    function name() external view returns (string memory);
+
+    function symbol() external view returns (string memory);
+
+    function decimals() external view returns (uint8);
+
+    function balanceOf(address) external view returns (uint256);
+
     function strikeAsset() external view returns (address);
 
-    function strikeAssetDecimals() external view returns (uint8 strikePriceDecimals);
+    function strikeAssetDecimals() external view returns (uint8);
 
     function underlyingAsset() external view returns (address);
 
-    function underlyingAssetDecimals() external view returns (uint8 underlyingAssetDecimals);
+    function underlyingAssetDecimals() external view returns (uint8);
 
-    function strikePrice() external view returns (uint8 strikePrice);
+    function strikePrice() external view returns (uint256);
 
-    function strikePriceDecimals() external view returns (uint8 strikePriceDecimals);
+    function strikePriceDecimals() external view returns (uint8);
 
-    function lockedBalance(address) external view returns (uint256 lockedBalance);
+    function lockedBalance(address) external view returns (uint256);
 
     function expirationBlockNumber() external view returns (uint256);
+
+    function underlyingBalance() external view returns (uint256);
+
+    function strikeBalance() external view returns (uint256);
+
+    function hasExpired() external view returns (bool);
 }
