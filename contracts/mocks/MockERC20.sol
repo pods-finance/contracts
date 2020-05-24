298ccb5388a49d6ad6d2ef28022d088485eea17d
Robsonsjre <robsonsjre@gmail.com>
2020-05-24 15:39:09 -0300

    created wBTC kovan mock to mirror mainnet

--- contracts/mocks/MockERC20.sol
+++ contracts/mocks/MockERC20.sol
@@ -8,8 +8,10 @@ contract MockERC20 is ERC20 {
     constructor(
         string memory _name,
         string memory _symbol,
-        uint8 _decimals
+        uint8 _decimals,
+        uint256 amountToMint
     ) public ERC20(_name, _symbol) {
         _setupDecimals(_decimals);
+        _mint(msg.sender, amountToMint);
     }
 }
