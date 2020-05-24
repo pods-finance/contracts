a1cbe32bb06b59884397e963c1d7db2bc7a4a11f
Robsonsjre <robsonsjre@gmail.com>
2020-05-24 15:41:01 -0300

    added migration script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,12 @@
 var PodToken = artifacts.require('./PodToken')
 var aPodToken = artifacts.require('./aPodToken')
+var MockERC20 = artifacts.require('./MockERC20')
+
 const getMonthLetter = require('../utils/utils.js')
 
 const PodTokenParams = {
   strikeAddress: '0xe22da380ee6B445bb8273C81944ADEB6E8450422',
-  underlyingAddress: '0x3b92f58feD223E2cB1bCe4c286BD97e42f2A12EA',
+  underlyingAddress: '0x1D77879843A507Ec1687cA7d06ED5e39bfb73fb4',
   underlyingSymbol: 'WBTC',
   strikeSymbol: 'USDC',
   strikePriceSymbol: 5000,
@@ -16,7 +18,7 @@ const PodTokenParams = {
 
 const PodTokenParams2 = {
   strikeAddress: '0x02f626c6ccb6d2ebc071c068dc1f02bf5693416a',
-  underlyingAddress: '0x3b92f58feD223E2cB1bCe4c286BD97e42f2A12EA',
+  underlyingAddress: '0x1D77879843A507Ec1687cA7d06ED5e39bfb73fb4',
   underlyingSymbol: 'WBTC',
   strikeSymbol: 'aUSDC',
   strikePriceSymbol: 5000,
@@ -36,7 +38,30 @@ PodTokenParams2.name = `pod:${PodTokenParams2.underlyingSymbol}:${PodTokenParams
 
 module.exports = function (deployer, network) {
   console.log('network', network)
-  deployer.deploy(PodToken, PodTokenParams.name, PodTokenParams.symbol, PodTokenParams.underlyingAddress, PodTokenParams.strikeAddress, PodTokenParams.strikePrice, PodTokenParams.expirationDate)
+  deployer.deploy(
+    PodToken, PodTokenParams.name,
+    PodTokenParams.symbol,
+    PodTokenParams.underlyingAddress,
+    PodTokenParams.strikeAddress,
+    PodTokenParams.strikePrice,
+    PodTokenParams.expirationDate
+  )
 
-  deployer.deploy(aPodToken, PodTokenParams2.name, PodTokenParams2.symbol, PodTokenParams2.underlyingAddress, PodTokenParams2.strikeAddress, PodTokenParams2.strikePrice, PodTokenParams2.expirationDate)
+  deployer.deploy(
+    aPodToken,
+    PodTokenParams2.name,
+    PodTokenParams2.symbol,
+    PodTokenParams2.underlyingAddress,
+    PodTokenParams2.strikeAddress,
+    PodTokenParams2.strikePrice,
+    PodTokenParams2.expirationDate
+  )
+
+  // deployer.deploy(
+  //   MockERC20,
+  //   'Wrapped BTC',
+  //   'WBTC',
+  //   8,
+  //   1000e8
+  // )
 }
