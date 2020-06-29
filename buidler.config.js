301e1b8d86111bbc9a32a6b440c19538ebebc989
Robsonsjre <robsonsjre@gmail.com>
2020-06-29 09:41:04 -0300

    removed web3 plugin

--- buidler.config.js
+++ buidler.config.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 
 usePlugin('@nomiclabs/buidler-waffle')
-usePlugin('@nomiclabs/buidler-web3')
 // This is a sample Buidler task. To learn how to create your own go to
 // https://buidler.dev/guides/create-task.html
 task('accounts', 'Prints the list of accounts', async () => {
