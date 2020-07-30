13718029ec836e349bb7e0d4394b912306700fbc
Rob <robsonsjre@gmail.com>
2020-07-30 15:55:10 -0300

    Features/add dai as collateral (#33)
    * deploy local enviroment done
    
    * updated factory kovan address
    
    * few adjusts on newSerie
    
    * update faucet
    
    * add aDai address to kovan deployment

--- contracts/mocks/faucet.sol
+++ contracts/mocks/faucet.sol
@@ -14,34 +14,43 @@ interface LendingPool {
 
 contract FaucetKovan {
     using SafeMath for uint256;
+    address aaveUSDCAddress = 0xe22da380ee6B445bb8273C81944ADEB6E8450422;
+    address aaveDAIAddress = 0xFf795577d9AC8bD7D90Ee22b6C1703490b6512FD;
+    address aDAIAddress = 0x58AD4cB396411B691A9AAb6F74545b2C5217FE6a;
+    address aUSDCAddress = 0x02F626c6ccb6D2ebC071c068DC1f02Bf5693416a;
+    address lendingPoolAddress = 0x580D4Fdc4BF8f9b5ae2fb9225D584fED4AD5375c;
+    address lendingPoolCoreAddress = 0x95D1189Ed88B380E319dF73fF00E479fcc4CFa45;
+    address kovanWBTCAddress = 0x351a448d49C8011D293e81fD53ce5ED09F433E4c;
+    LendingPool lendingPool = LendingPool(lendingPoolAddress);
+    ERC20Mintable usdc = ERC20Mintable(aaveUSDCAddress);
+    ERC20Mintable dai = ERC20Mintable(aaveDAIAddress);
+    ERC20Mintable wbtc = ERC20Mintable(kovanWBTCAddress);
 
     function getFaucet() external {
-        address aaveUSDCAddress = 0xe22da380ee6B445bb8273C81944ADEB6E8450422;
-        address aUSDCAddress = 0x02F626c6ccb6D2ebC071c068DC1f02Bf5693416a;
-        address lendingPoolAddress = 0x580D4Fdc4BF8f9b5ae2fb9225D584fED4AD5375c;
-        address lendingPoolCoreAddress = 0x95D1189Ed88B380E319dF73fF00E479fcc4CFa45;
-        address kovanWBTCAddress = 0x351a448d49C8011D293e81fD53ce5ED09F433E4c;
-
-        ERC20Mintable usdc = ERC20Mintable(aaveUSDCAddress);
-        ERC20Mintable wbtc = ERC20Mintable(kovanWBTCAddress);
-
-        uint256 usdcAskedAmount = 30000;
+        uint256 askedAmount = 30000;
         uint8 usdcDecimals = usdc.decimals();
-        uint256 mintedUsdcAmount = usdcAskedAmount.mul(10**uint256(usdcDecimals));
+        uint8 daiDecimals = dai.decimals();
+        uint256 mintedUsdcAmount = askedAmount.mul(10**uint256(usdcDecimals));
+        uint256 mintedDaiAmount = askedAmount.mul(10**uint256(daiDecimals));
         // Mint USDC
         usdc.mint(mintedUsdcAmount);
+        dai.mint(mintedDaiAmount);
         usdc.transfer(msg.sender, mintedUsdcAmount.div(2));
+        dai.transfer(msg.sender, mintedDaiAmount.div(2));
 
         // Mint aUSDC
         usdc.approve(lendingPoolCoreAddress, mintedUsdcAmount.div(2));
+        dai.approve(lendingPoolCoreAddress, mintedDaiAmount.div(2));
         LendingPool(lendingPoolAddress).deposit(aaveUSDCAddress, mintedUsdcAmount.div(2), 0);
+        LendingPool(lendingPoolAddress).deposit(aaveDAIAddress, mintedDaiAmount.div(2), 0);
 
         // send Aave aUSDC
         ERC20Mintable(aUSDCAddress).transfer(msg.sender, mintedUsdcAmount.div(2));
+        ERC20Mintable(aDAIAddress).transfer(msg.sender, mintedDaiAmount.div(2));
 
-        uint256 askedAmount = 100;
+        uint256 askedWbtcAmount = 100;
         uint8 wbtcDecimals = wbtc.decimals();
-        uint256 mintedWbtcAmount = askedAmount.mul(10**uint256(wbtcDecimals));
+        uint256 mintedWbtcAmount = askedWbtcAmount.mul(10**uint256(wbtcDecimals));
         // Mint WBTC
         wbtc.mint(mintedWbtcAmount);
         wbtc.transfer(msg.sender, mintedWbtcAmount);
