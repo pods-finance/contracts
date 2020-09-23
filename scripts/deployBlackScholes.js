8f1149f1dfad7e8e06c4aae8ecc75ce87072e411
Guilherme Viana <ggviana@hotmail.com.br>
2020-09-23 00:14:58 -0300

    Added deploy script

---
+++ scripts/deployBlackScholes.js
@@ -0,0 +1,40 @@
+const bre = require('@nomiclabs/buidler')
+const getContractFactoryWithLibraries = require('../test/util/getContractFactoryWithLibraries')
+
+async function main () {
+  const FixidityLib = await ethers.getContractFactory('FixidityLib')
+  const fixidity = await FixidityLib.deploy()
+  await fixidity.deployed()
+
+  const LogarithmLib = await getContractFactoryWithLibraries('LogarithmLib', {
+    FixidityLib: fixidity.address
+  })
+  const logarithm = await LogarithmLib.deploy()
+  await logarithm.deployed()
+
+  const ExponentLib = await getContractFactoryWithLibraries('ExponentLib', {
+    FixidityLib: fixidity.address,
+    LogarithmLib: logarithm.address
+  })
+  const exponent = await ExponentLib.deploy()
+  await exponent.deployed()
+
+  const BlackScholes = await getContractFactoryWithLibraries('BlackScholes', {
+    FixidityLib: fixidity.address,
+    LogarithmLib: logarithm.address,
+    ExponentLib: exponent.address,
+  })
+
+  const bs = await BlackScholes.deploy()
+  await bs.deployed()
+  console.log('BlackScholes Address: ', bs.address)
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
