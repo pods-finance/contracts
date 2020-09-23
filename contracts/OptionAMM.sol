debb98380be8ad8ffc8dc938ba4670f40a4c8677
Robsonsjre <robsonsjre@gmail.com>
2020-09-23 20:43:57 -0300

    fixed some compiling bugs

--- contracts/OptionAMM.sol
+++ contracts/OptionAMM.sol
@@ -1,9 +1,17 @@
 pragma solidity ^0.6.8;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
 contract OptionAMM {
+    using SafeMath for uint256;
+
     uint256 currentSigma;
     address option;
     address stableAsset;
+    address underlyingAsset;
+    uint256 expiration;
+    uint256 strikePrice;
     uint256 deamortizedOptionBalance;
     uint256 deamortizedStableBalance;
     uint256 fImp;
@@ -20,6 +28,8 @@ contract OptionAMM {
     /** Events */
     event AddLiquidity(address indexed caller, uint256 amountOfStable, uint256 amountOfOptions);
     event RemoveLiquidity(address indexed caller, uint256 amountOfStable, uint256 amountOfOptions);
+    event BuyExact(address indexed caller, uint256 amount);
+    event SellExact(address indexed caller, uint256 amount);
 
     constructor(address _optionAddress, address _stableAsset) public {
         stableAsset = _stableAsset;
@@ -28,8 +38,8 @@ contract OptionAMM {
 
     function addLiquidity(uint256 amountOfStable, uint256 amountOfOptions) public {
         // 2) Calculate Totals
-        uint256 totalStable = ERC20(strikeAsset).balanceOf(address(this));
-        uint256 totalOptions = ERC20(option).balanceOf(address(this));
+        uint256 totalStable = IERC20(stableAsset).balanceOf(address(this));
+        uint256 totalOptions = IERC20(option).balanceOf(address(this));
 
         bool isInitialLiquidity = totalStable == 0 || totalOptions == 0;
         uint256 fImpOpening;
@@ -41,10 +51,10 @@ contract OptionAMM {
             deamortizedOptionBalance = amountOfOptions;
             deamortizedStableBalance = amountOfStable;
         } else {
-            uint256 spotPrice = CHAINLINK(option.underlyingAsset, option.expiration);
+            uint256 spotPrice = CHAINLINK(underlyingAsset);
             // 1. new Calculated BS Price => new spot, new time, last sigma
-            uint256 timeToMaturity = option.expiration - block.timestamp;
-            uint256 newPrice = BS(spotPrice, option.strikePrice, currentSigma, timeToMaturity, riskFree);
+            uint256 timeToMaturity = expiration - block.timestamp;
+            uint256 newPrice = BS(spotPrice, strikePrice, currentSigma, timeToMaturity, riskFree);
 
             // 2) FImpOpening(balanceOf(A), balanceOf(B), amortizedBalance(A), amortizedBalance(B))
             // fImp = (totalOptions*spotPrice + totalStable) / (deamortizedOption*spotPrice + deamortizedStable)
@@ -64,42 +74,126 @@ contract OptionAMM {
 
         // 5. transferFrom(amountA) / transferFrom(amountB) = > Already updates the new balanceOf(a) / balanceOf(b)
         require(
-            ERC20(optionAddress).transferFrom(msg.sender, address(this), amountOfOptions),
+            IERC20(option).transferFrom(msg.sender, address(this), amountOfOptions),
             "Could not transfer option tokens from caller"
         );
 
         require(
-            ERC20(strikeAsset).transferFrom(msg.sender, address(this), amountOfStable),
+            IERC20(stableAsset).transferFrom(msg.sender, address(this), amountOfStable),
             "Could not transfer stable tokens from caller"
         );
 
-        emit AddedLiquidity(msg.sender, amountOfOptions, amountOfStable);
+        emit AddLiquidity(msg.sender, amountOfOptions, amountOfStable);
     }
 
     function removeLiquidity(uint256 amountOfStable, uint256 amountOfOptions) public {
         // 2) Calculate Totals
-        uint256 totalStable = ERC20(strikeAsset).balanceOf(address(this));
-        uint256 totalOptions = ERC20(option).balanceOf(address(this));
+        uint256 totalStable = IERC20(stableAsset).balanceOf(address(this));
+        uint256 totalOptions = IERC20(option).balanceOf(address(this));
 
-        uint256 spotPrice = CHAINLINK(option.underlyingAsset, option.expiration);
+        uint256 spotPrice = CHAINLINK(underlyingAsset);
         // 1. new Calculated BS Price => new spot, new time, last sigma
-        uint256 timeToMaturity = option.expiration - block.timestamp;
-        uint256 newPrice = BS(spotPrice, option.strikePrice, currentSigma, timeToMaturity, riskFree);
+        uint256 timeToMaturity = expiration - block.timestamp;
+        uint256 newPrice = BS(spotPrice, strikePrice, currentSigma, timeToMaturity, riskFree);
 
         // 2) FImpOpening(balanceOf(A), balanceOf(B), amortizedBalance(A), amortizedBalance(B))
         // fImp = (totalOptions*spotPrice + totalStable) / (deamortizedOption*spotPrice + deamortizedStable)
-        fImpOpening = totalOptions.mul(spotPrice).add(totalStable).div(
+        uint256 fImpOpening = totalOptions.mul(spotPrice).add(totalStable).div(
             deamortizedOptionBalance.mul(spotPrice).add(deamortizedStableBalance)
         );
 
         // 3) Calculate rescue multipliers
-        uint256 depositedImp = balances[msg.sender].fImp;
 
-        uint256 auxStableStable = (Min(deamortizedStableBalance.mul(fImpOpening), totalStable)).div(
+        (uint256 optionAmountAvaiableToWithdraw, uint256 stableAmountAvaiableToWithdraw) = calculateAvaiableAmount(
+            deamortizedStableBalance,
+            deamortizedOptionBalance,
+            totalStable,
+            totalOptions,
+            amountOfOptions,
+            amountOfStable,
+            fImpOpening,
+            balances[msg.sender].fImp
+        );
+
+        require(
+            amountOfStable < stableAmountAvaiableToWithdraw && amountOfOptions < optionAmountAvaiableToWithdraw,
+            "You cant remove those quantities"
+        );
+
+        // 4) Update users properties
+        balances[msg.sender].optionBalance = balances[msg.sender].optionBalance.sub(amountOfOptions);
+        balances[msg.sender].stableBalance = balances[msg.sender].optionBalance.sub(amountOfStable);
+        // 5) Generate impact on multipliers
+
+        //6) Update deamortizedBalances
+        deamortizedOptionBalance = deamortizedOptionBalance.sub(amountOfOptions.div(fImpOpening));
+        deamortizedStableBalance = deamortizedStableBalance.sub(amountOfStable.div(fImpOpening));
+
+        // 5. transferFrom(amountA) / transferFrom(amountB) = > Already updates the new balanceOf(a) / balanceOf(b)
+        require(IERC20(option).transfer(msg.sender, amountOfOptions), "Could not transfer option tokens from caller");
+
+        require(
+            IERC20(stableAsset).transfer(msg.sender, amountOfStable),
+            "Could not transfer stable tokens from caller"
+        );
+
+        emit RemoveLiquidity(msg.sender, amountOfOptions, amountOfStable);
+    }
+
+    function buyExact(
+        uint256 maxPayedStable,
+        uint256 amount,
+        uint256 sigmaInitialGuess
+    ) public {
+        // 1) Calculate BS
+        // 1a) Consult spotPrice Oracle
+        uint256 spotPrice = CHAINLINK(underlyingAsset); //
+        uint256 timeToMaturity = expiration - block.timestamp; //expiration or endOfExerciseWindow
+        uint256 newPrice = BS(spotPrice, strikePrice, currentSigma, timeToMaturity, riskFree); //riskFree = 0
+
+        // 2) Calculate Totals
+        uint256 totalStable = IERC20(stableAsset).balanceOf(address(this));
+        uint256 totalOptions = IERC20(option).balanceOf(address(this));
+
+        // 2a) Calculate Avaiable Pools
+        uint256 poolOptions = min(totalOptions, totalStable.div(newPrice));
+        uint256 poolStable = min(totalStable, totalOptions.mul(newPrice));
+
+        // 2c) Product Constant
+        uint256 productConstant = poolOptions.mul(poolStable);
+
+        // 3. Calculate Paying/Receiving money => [(2c) / ((2) +/- amount) - (2b)]
+        uint256 stableToTransfer = productConstant.div(poolOptions.sub(amount)).sub(poolStable);
+
+        //3b. Calculate price per option => (3)/ amount
+        uint256 targetPrice = stableToTransfer.div(amount);
+
+        // 4. Update currentSigma
+        currentSigma = findNextSigma(targetPrice, sigmaInitialGuess, currentSigma, newPrice);
+
+        // 5. transfer assets
+        require(IERC20(stableAsset).transferFrom(msg.sender, address(this), stableToTransfer), "not transfered asset");
+
+        require(IERC20(option).transfer(msg.sender, amount), "not transfered asset");
+
+        emit BuyExact(msg.sender, amount);
+    }
+
+    function calculateAvaiableAmount(
+        uint256 deamortizedStableBalance,
+        uint256 deamortizedOptionBalance,
+        uint256 totalStable,
+        uint256 totalOptions,
+        uint256 amountOfOptions,
+        uint256 amountOfStable,
+        uint256 fImpOpening,
+        uint256 depositedImp
+    ) internal view returns (uint256, uint256) {
+        uint256 auxStableStable = (min(deamortizedStableBalance.mul(fImpOpening), totalStable)).div(
             deamortizedStableBalance
         );
 
-        uint256 auxOptionOption = (Min(deamortizedOptionBalance.mul(fImpOpening), totalOption)).div(
+        uint256 auxOptionOption = (min(deamortizedOptionBalance.mul(fImpOpening), totalOptions)).div(
             deamortizedOptionBalance
         );
 
@@ -116,78 +210,36 @@ contract OptionAMM {
         uint256 MOptionStable = amountOfOptions.mul(auxOptionStable).div(depositedImp);
         uint256 MOptionOption = amountOfOptions.mul(auxOptionOption).div(depositedImp);
 
-        uint256 optionAmountAvaiableToWithdraw = MOptionOption.add(MStableOption);
-        uint256 stableAmountAvaiableToWithdraw = MStableStable.add(MOptionStable);
-
-        require(
-            amountOfStable < stableAmountAvaiableToWithdraw && amountOfOptions < optionAmountAvaiableToWithdraw,
-            "You cant remove those quantities"
-        );
-
-        // 4) Update users properties
-        balances[msg.sender].optionBalance = balances[msg.sender].optionBalance.sub(amountOfOptions);
-        balances[msg.sender].stableBalance = balances[msg.sender].optionBalance.sub(amountOfStable);
-        // 5) Generate impact on multipliers
-
-        
-
-        //6) Update deamortizedBalances
-        deamortizedOptionBalance = deamortizedOptionBalance.sub(amountOfOptions.div(fImpOpening));
-        deamortizedStableBalance = deamortizedStableBalance.sub(amountOfStable.div(fImpOpening));
-
-        // 5. transferFrom(amountA) / transferFrom(amountB) = > Already updates the new balanceOf(a) / balanceOf(b)
-        require(
-            ERC20(optionAddress).transfer(msg.sender, amountOfOptions),
-            "Could not transfer option tokens from caller"
-        );
-
-        require(
-            ERC20(strikeAsset).transfer(msg.sender, amountOfStable),
-            "Could not transfer stable tokens from caller"
-        );
+        return (MOptionOption.add(MStableOption), MStableStable.add(MOptionStable));
     }
 
-    function buyExact(
-        uint256 maxPayedStable,
-        uint256 amount,
-        uint256 sigmaInitialGuess
-    ) public {
-        // 1) Calculate BS
-        // 1a) Consult spotPrice Oracle
-        uint256 spotPrice = CHAINLINK(option.underlyingAsset, option.expiration); //
-        uint256 timeToMaturity = deltaTime(block.timestamp, option.expiration); //expiration or endOfExerciseWindow
-        uint256 newPrice = BS(spotPrice, option.strikePrice, currentSigma, timeToMaturity, riskFree); //riskFree = 0
-
-        // 2) Calculate Totals
-        uint256 totalStable = ERC20(strikeAsset).balanceOf(address(this));
-        uint256 totalOptions = ERC20(option).balanceOf(address(this));
-
-        // 2a) Calculate Avaiable Pools
-        uint256 poolOptions = Min(totalOption, totalStable.div(newPrice));
-        uint256 poolStable = Min(totalStable, totalOptions.mul(newPrice));
-
-        // 2c) Product Constant
-        uint256 productConstant = poolOptions.mul(poolStable);
-
-        // 3. Calculate Paying/Receiving money => [(2c) / ((2) +/- amount) - (2b)]
-        uint256 stableToTransfer = productConstant.div(poolOptions.sub(amount)).sub(poolStable);
-
-        //3b. Calculate price per option => (3)/ amount
-        uint256 targetPrice = stableToTransfer.div(amount);
-
-        // 4. Update currentSigma
-        currentSigma = findNextSigma(targetPrice, sigmaInitialGuess, currentSigma, newPrice);
-
-        // 5. transfer assets
-        require(
-            ERC20(option.strikeAsset).transferFrom(msg.sender, address(this), stableToTransfer),
-            "not transfered asset"
-        );
-
-        require(ERC20(option.option).transfer(msg.sender, amount), "not transfered asset");
+    function BS(
+        uint256 spotPrice,
+        uint256 strikePrice,
+        uint256 sigma,
+        uint256 daysRemaining,
+        uint256 riskFree
+    ) public view returns (uint256) {
+        return 2;
     }
 
-    // function sellExact() public {
+    function CHAINLINK(address asset) public view returns (uint256 price) {
+        price = 2;
+    }
 
-    // }
+    function findNextSigma(
+        uint256 targetPrice,
+        uint256 sigmaInitialGuess,
+        uint256 currentSigma,
+        uint256 lastPrice
+    ) public view returns (uint256) {
+        return 2;
+    }
+
+    /**
+     * @dev Returns the smallest of two numbers.
+     */
+    function min(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a < b ? a : b;
+    }
 }
