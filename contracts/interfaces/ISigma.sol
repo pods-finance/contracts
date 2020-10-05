2a94a3f9ef4d495299f1f64de301fe4e35efb113
Robsonsjre <robsonsjre@gmail.com>
2020-10-04 22:35:44 -0300

    added Sigma interface

---
+++ contracts/interfaces/ISigma.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.8;
+
+// TODO: add call other methods
+interface ISigma {
+    function blackScholes() external returns (address);
+
+    function getPutSigma(
+        uint256 _targetPrice,
+        uint256 _sigmaInitialGuess,
+        uint256 _spotPrice,
+        uint256 _strikePrice,
+        uint256 _timeToMaturity,
+        uint256 _riskFree
+    ) external view returns (uint256, uint256);
+}
