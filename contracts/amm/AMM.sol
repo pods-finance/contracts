6b8dec28d8757ffec759f71de2626fa556739cba
Robsonsjre <robsonsjre@gmail.com>
2020-10-07 22:15:32 -0300

    remove liquidity basic version working

--- contracts/amm/AMM.sol
+++ contracts/amm/AMM.sol
@@ -3,12 +3,13 @@ pragma solidity ^0.6.8;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@nomiclabs/buidler/console.sol";
 
 abstract contract AMM {
     using SafeMath for uint256;
 
-    uint256 constant INITIAL_FIMP = 10**18;
+    uint256 constant INITIAL_FIMP = 10**27;
+    uint256 constant FIMP_PRECISION = 27;
+    uint256 constant MULT_PRECISION = 27;
     uint32 constant WAD_DECIMALS = 18;
 
     // Constructor Info
@@ -76,14 +77,18 @@ abstract contract AMM {
 
             // 2) FImpOpening(balanceOf(A), balanceOf(B), amortizedBalance(A), amortizedBalance(B))
             // fImp = (totalOptions*spotPrice + totalStable) / (deamortizedOption*spotPrice + deamortizedStable)
-            fImpOpening = totalTokenA.mul(ABPrice).add(totalTokenB).div(
-                deamortizedTokenABalance.mul(ABPrice).add(deamortizedTokenBBalance)
+            fImpOpening = _getFImpOpening(
+                totalTokenA,
+                totalTokenB,
+                ABPrice,
+                deamortizedTokenABalance,
+                deamortizedTokenBBalance
             );
 
             // 4) Update demortizedBalances;
             // deamortizedBalance = deamortizedBalance + amount/fImpOpening
-            deamortizedTokenABalance = deamortizedTokenABalance.add(amountOfA.div(fImpOpening));
-            deamortizedTokenBBalance = deamortizedTokenBBalance.add(amountOfB.div(fImpOpening));
+            deamortizedTokenABalance = deamortizedTokenABalance.add(amountOfA.mul(10**FIMP_PRECISION).div(fImpOpening));
+            deamortizedTokenBBalance = deamortizedTokenBBalance.add(amountOfB.mul(10**FIMP_PRECISION).div(fImpOpening));
         }
 
         // 3) Update User properties (BalanceUserA, BalanceUserB, fImpMoment)
@@ -111,34 +116,47 @@ abstract contract AMM {
     }
 
     function removeLiquidity(uint256 amountOfAOriginal, uint256 amountOfBOriginal) public {
-        {
-            (uint256 userTokenABalance, uint256 userTokenBBalance) = _getUserBalances(msg.sender);
-            require(
-                amountOfAOriginal <= userTokenABalance && amountOfBOriginal <= userTokenBBalance,
-                "not enough original balance"
-            );
-
-            balances[msg.sender].tokenABalance = userTokenABalance.sub(amountOfAOriginal);
-            balances[msg.sender].tokenBBalance = userTokenBBalance.sub(amountOfBOriginal);
-
-            deamortizedTokenABalance = deamortizedTokenABalance.sub(amountOfAOriginal.div(balances[msg.sender].fImp));
-            deamortizedTokenBBalance = deamortizedTokenBBalance.sub(amountOfBOriginal.div(balances[msg.sender].fImp));
-        }
+        (uint256 userTokenABalance, uint256 userTokenBBalance) = _getUserBalances(msg.sender);
+        require(
+            amountOfAOriginal <= userTokenABalance && amountOfBOriginal <= userTokenBBalance,
+            "not enough original balance"
+        );
 
         (uint256 totalTokenA, uint256 totalTokenB) = _getPoolBalances();
+
         // 1) Spot Price
         // How many B you need in order to exchange for 1 unit of A
         uint256 ABPrice = _getABPrice();
+
         // 2) FImpOpening(balanceOf(A), balanceOf(B), amortizedBalance(A), amortizedBalance(B))
         // fImp = (totalOptions*spotPrice + totalStable) / (deamortizedOption*spotPrice + deamortizedStable)
-        uint256 fImpOpening = totalTokenA.mul(ABPrice).add(totalTokenB).div(
-            deamortizedTokenABalance.mul(ABPrice).add(deamortizedTokenBBalance)
+        uint256 fImpOpening = _getFImpOpening(
+            totalTokenA,
+            totalTokenB,
+            ABPrice,
+            deamortizedTokenABalance,
+            deamortizedTokenBBalance
         );
 
         Mult memory multipliers = _getMultipliers(totalTokenA, totalTokenB, fImpOpening);
 
-        uint256 amountToSendA = amountOfAOriginal.mul(multipliers.AA).add(amountOfBOriginal.mul(multipliers.BA));
-        uint256 amountToSendB = amountOfBOriginal.mul(multipliers.BB).add(amountOfAOriginal.mul(multipliers.AB));
+        balances[msg.sender].tokenABalance = userTokenABalance.sub(amountOfAOriginal);
+        balances[msg.sender].tokenBBalance = userTokenBBalance.sub(amountOfBOriginal);
+
+        deamortizedTokenABalance = deamortizedTokenABalance.sub(
+            amountOfAOriginal.mul(10**FIMP_PRECISION).div(balances[msg.sender].fImp)
+        );
+        deamortizedTokenBBalance = deamortizedTokenBBalance.sub(
+            amountOfBOriginal.mul(10**FIMP_PRECISION).div(balances[msg.sender].fImp)
+        );
+
+        // (amountOfAOriginal*AA + amountOfBOriginal*BA) / fImpUser
+        uint256 amountToSendA = amountOfAOriginal.mul(multipliers.AA).add(amountOfBOriginal.mul(multipliers.BA)).div(
+            balances[msg.sender].fImp
+        );
+        uint256 amountToSendB = amountOfBOriginal.mul(multipliers.BB).add(amountOfAOriginal.mul(multipliers.AB)).div(
+            balances[msg.sender].fImp
+        );
 
         // 5. transferFrom(amountA) / transferFrom(amountB) = > Already updates the new balanceOf(a) / balanceOf(b)
         require(ERC20(tokenA).transfer(msg.sender, amountToSendA), "Could not transfer token A from caller");
@@ -167,39 +185,76 @@ abstract contract AMM {
         return amountOfTokenBOut;
     }
 
+    function _getFImpOpening(
+        uint256 _totalTokenA,
+        uint256 _totalTokenB,
+        uint256 _ABPrice,
+        uint256 _deamortizedTokenABalance,
+        uint256 _deamortizedTokenBBalance
+    ) internal pure returns (uint256) {
+        uint256 fImpOpening = _totalTokenA.mul(_ABPrice).add(_totalTokenB).mul(10**FIMP_PRECISION).div(
+            _deamortizedTokenABalance.mul(_ABPrice).add(_deamortizedTokenBBalance)
+        );
+        return fImpOpening;
+    }
+
     // function _buyExactTokensWithTokens(uint256 maxAmountOfTokensA, uint256 amountOfTokensB) internal virtual;
 
     function _getPoolBalances() internal view returns (uint256, uint256) {
         uint256 balanceOfTokenA = ERC20(tokenA).balanceOf(address(this));
-        uint256 normalizedBalanceA = balanceOfTokenA.mul(10**(uint256(WAD_DECIMALS - tokenADecimals)));
+        // uint256 normalizedBalanceA = balanceOfTokenA.mul(10**(uint256(WAD_DECIMALS - tokenADecimals)));
 
         uint256 balanceOfTokenB = ERC20(tokenB).balanceOf(address(this));
-        uint256 normalizedBalanceB = balanceOfTokenB.mul(10**(uint256(WAD_DECIMALS - tokenBDecimals)));
+        // uint256 normalizedBalanceB = balanceOfTokenB.mul(10**(uint256(WAD_DECIMALS - tokenBDecimals)));
 
-        return (normalizedBalanceA, normalizedBalanceB);
+        // return (normalizedBalanceA, normalizedBalanceB);
+        return (balanceOfTokenA, balanceOfTokenB);
     }
 
     function _getUserBalances(address user) internal view returns (uint256, uint256) {
         uint256 balanceOfTokenA = balances[user].tokenABalance;
-        uint256 normalizedBalanceA = balanceOfTokenA.mul(10**(uint256(WAD_DECIMALS - tokenADecimals)));
+        // uint256 normalizedBalanceA = balanceOfTokenA.mul(10**(uint256(WAD_DECIMALS - tokenADecimals)));
 
         uint256 balanceOfTokenB = balances[user].tokenBBalance;
-        uint256 normalizedBalanceB = balanceOfTokenB.mul(10**(uint256(WAD_DECIMALS - tokenBDecimals)));
+        // uint256 normalizedBalanceB = balanceOfTokenB.mul(10**(uint256(WAD_DECIMALS - tokenBDecimals)));
 
-        return (normalizedBalanceA, normalizedBalanceB);
+        // return (normalizedBalanceA, normalizedBalanceB);
+        return (balanceOfTokenA, balanceOfTokenB);
     }
 
     function _getMultipliers(
         uint256 totalTokenA,
         uint256 totalTokenB,
         uint256 fImpOpening
-    ) internal view returns (Mult memory multipliers) {
-        uint256 mAA = (min(deamortizedTokenABalance.mul(fImpOpening), totalTokenA)).div(deamortizedTokenABalance);
-        uint256 mBB = (min(deamortizedTokenBBalance.mul(fImpOpening), totalTokenB)).div(deamortizedTokenBBalance);
-        uint256 mAB = totalTokenB.sub(mBB.mul(deamortizedTokenBBalance)).div(deamortizedTokenABalance);
-        uint256 mBA = totalTokenA.sub(mAA.mul(deamortizedTokenABalance)).div(deamortizedTokenBBalance);
+    ) internal view returns (Mult memory) {
+        uint256 totalTokenAWithPrecision = totalTokenA.mul(10**FIMP_PRECISION);
+        uint256 totalTokenBWithPrecision = totalTokenB.mul(10**FIMP_PRECISION);
+        uint256 mAA = 0;
+        uint256 mBB = 0;
+        uint256 mAB = 0;
+        uint256 mBA = 0;
 
-        multipliers = Mult(mAA, mBB, mAB, mBA);
+        if (deamortizedTokenABalance > 0) {
+            mAA = (min(deamortizedTokenABalance.mul(fImpOpening), totalTokenAWithPrecision)).div(
+                deamortizedTokenABalance
+            );
+        }
+
+        if (deamortizedTokenBBalance > 0) {
+            mBB = (min(deamortizedTokenBBalance.mul(fImpOpening), totalTokenBWithPrecision)).div(
+                deamortizedTokenBBalance
+            );
+        }
+        if (mAA > 0) {
+            mAB = totalTokenBWithPrecision.sub(mBB.mul(deamortizedTokenBBalance)).div(deamortizedTokenABalance);
+        }
+
+        if (mBB > 0) {
+            mBA = totalTokenAWithPrecision.sub(mAA.mul(deamortizedTokenABalance)).div(deamortizedTokenBBalance);
+        }
+
+        Mult memory multipliers = Mult(mAA, mAB, mBA, mBB);
+        return multipliers;
     }
 
     function _getAvaiableForRescueAmounts(
