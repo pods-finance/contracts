69b30c8946a8e881b53b14a1575818778e1820be
Robsonsjre <robsonsjre@gmail.com>
2020-10-12 22:24:54 -0300

    fixed price calculation mockAMM

---
+++ test/scenarios/APR.js
@@ -0,0 +1,95 @@
+// const scenario = {
+//   descripition: 'esse eh o primeiro cenario dentro de APR',
+//   actions: [
+//     {
+//       name: 'mint',
+//       contract: 'mockTokenA',
+//       user: user00,
+//       params: [amountOfTokenAUser00]
+//     },
+//     {
+//       name: 'mint',
+//       contract: mockTokenB,
+//       user: user00,
+//       params: [amountOfTokenBUser00]
+//     },
+//     {
+//       name: 'mint',
+//       contract: mockTokenA,
+//       user: user01,
+//       params: [amountOfTokenAUser01]
+//     },
+//     {
+//       name: 'mint',
+//       contract: mockTokenB,
+//       user: user02,
+//       params: [amountOfTokenBUser02]
+//     },
+//     {
+//       name: 'approve',
+//       contract: mockTokenA,
+//       user: user00,
+//       params: [amm.address, amountOfTokenAUser00]
+//     },
+//     {
+//       name: 'approve',
+//       contract: mockTokenB,
+//       user: user00,
+//       params: [amm.address, amountOfTokenBUser00]
+//     },
+//     {
+//       name: 'approve',
+//       contract: mockTokenA,
+//       user: user01,
+//       params: [amm.address, amountOfTokenAUser01]
+//     },
+//     {
+//       name: 'approve',
+//       contract: mockTokenB,
+//       user: user02,
+//       params: [amm.address, amountOfTokenBUser02]
+//     },
+//     {
+//       name: 'setPrice',
+//       contract: amm,
+//       user: user00,
+//       params: [tokenPrice00]
+//     },
+//     {
+//       name: 'addLiquidity',
+//       contract: amm,
+//       user: user00,
+//       params: [amountOfTokenAUser00, amountOfTokenBUser00]
+//     },
+//     {
+//       name: 'addLiquidity',
+//       contract: amm,
+//       user: user01,
+//       params: [amountOfTokenAUser01, 0]
+//     },
+//     {
+//       name: 'addLiquidity',
+//       contract: amm,
+//       user: user02,
+//       params: [0, amountOfTokenBUser02]
+//     },
+//     {
+//       name: 'removeLiquidity',
+//       contract: amm,
+//       user: user01,
+//       params: [amountOfTokenAUser01, 0]
+//     },
+//     {
+//       name: 'removeLiquidity',
+//       contract: amm,
+//       user: user00,
+//       params: [amountOfTokenAUser00, amountOfTokenBUser00]
+//     },
+//     {
+//       name: 'removeLiquidity',
+//       contract: amm,
+//       user: user02,
+//       params: [0, amountOfTokenBUser02]
+//     }
+//   ]
+// }
