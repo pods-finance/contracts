836208f1c18d92d6cecfe002735967256301dac9
Robsonsjre <robsonsjre@gmail.com>
2020-10-26 02:42:09 -0300

    added docs for AMM

--- contracts/amm/AMM.sol
+++ contracts/amm/AMM.sol
@@ -4,13 +4,59 @@ pragma solidity ^0.6.8;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
+/**
+ * Represents a generalized contract for a single-sided AMM pair.
+ *
+ * That means is possible to add and remove liquidity in any proportion
+ * at any time, even 0 in one of the sides.
+ *
+ * The AMM is constituied by 3 core functions: Add Liquidity, Remove liquidity and Trade.
+ *
+ * There are 4 possible trade types between the token pair (tokenA and tokenB):
+ *
+ * - ExactAInput:
+ *     tokenA as an exact Input, meaning that the output tokenB is variable.
+ *     it is important to have a slippage control of the minimum acceptable amount of tokenB in return
+ * - ExactAOutput:
+ *     tokenA as an exact Output, meaning that the input tokenB is variable.
+ *     it is important to have a slippage control of the maximum acceptable amount of tokenB sent
+ * - ExactBInput:
+ *     tokenB as an exact Input, meaning that the output tokenA is variable.
+ *     it is important to have a slippage control of the minimum acceptable amount of tokenA in return
+ * - ExactBOutput:
+ *     tokenB as an exact Output, meaning that the input tokenA is variable.
+ *     it is important to have a slippage control of the maximum acceptable amount of tokenA sent
+ *
+ * Several functions are provided as virtual and must be overrieded by the inheritor.
+ *
+ * - _getABPrice:
+ *     function that will return the tokenA:tokenB price relation.
+ *     How many units of tokenB in order to traded for 1 unit of tokenA.
+ *     This price is represented in the same tokenB number of decimals.
+ * - _onAddLiquidity:
+ *     function that will be executed after balances updates and before
+ *     token transfers. Usually used for handling fees
+ * - _onRemoveLiquidity:
+ *     function that will be executed after balances updates and before
+ *     token transfers. Usually used for handling fees
+ * 
+ *  Also, for which TradeType (E.g: ExactAInput) there are more two functions to override:
+
+ * _getTradeDetails[$TradeType]:
+ *   This function is responsible to return the TradeDetails struct, that contains basically the amount
+ *   of the other token depending on the trade type. (E.g: ExactAInput => The TradeDetails will return the 
+ *   amount of B output).
+ * _onTrade[$TradeType]:
+*     function that will be executed after balances updates and before
+ *    token transfers. Usually used for handling fees and updating state at the inheritor.
+ *
+ */
+
 abstract contract AMM {
     using SafeMath for uint256;
 
     uint256 constant INITIAL_FIMP = 10**27;
     uint256 constant FIMP_PRECISION = 27;
-    uint256 constant MULT_PRECISION = 27;
-    uint32 constant WAD_DECIMALS = 18;
 
     // Constructor Info
     address public tokenA;
@@ -62,12 +108,20 @@ abstract contract AMM {
         tokenBDecimals = ERC20(_tokenB).decimals();
     }
 
+    /**
+     * _addLiquidity in any propotion of tokenA or tokenB
+     * The inheritor contract should implement _getABPrice and _onAddLiquidity functions
+     *
+     * @param amountOfA amount of TokenA to add
+     * @param amountOfB amount of TokenB to add
+     * @param owner address of the account that will have ownership of the liquidity
+     */
     function _addLiquidity(
         uint256 amountOfA,
         uint256 amountOfB,
         address owner
     ) internal {
-        // 2) Calculate Totals
+        // 1) Get Totals
         (uint256 totalTokenA, uint256 totalTokenB) = _getPoolBalances();
 
         bool isInitialLiquidity = totalTokenA == 0 || totalTokenB == 0;
@@ -76,6 +130,7 @@ abstract contract AMM {
         uint256 userAmountToStoreTokenB = amountOfB;
 
         if (isInitialLiquidity) {
+            // only in the initial liquidity is necessary add both tokens in any proportion
             require(amountOfA > 0 && amountOfB > 0, "You should add both tokens on the first liquidity");
 
             fImpOpening = INITIAL_FIMP;
@@ -114,7 +169,6 @@ abstract contract AMM {
 
         _onAddLiquidity(balances[owner], owner);
 
-        // 5. transferFrom(amountA) / transferFrom(amountB) = > Already updates the new balanceOf(a) / balanceOf(b)
         require(
             ERC20(tokenA).transferFrom(msg.sender, address(this), amountOfA),
             "Could not transfer option tokens from caller"
@@ -128,6 +182,13 @@ abstract contract AMM {
         emit AddLiquidity(msg.sender, owner, amountOfA, amountOfB);
     }
 
+    /**
+     * _removeLiquidity in any propotion of tokenA or tokenB
+     * The inheritor contract should implement _getABPrice and _onRemoveLiquidity functions
+     *
+     * @param amountOfAOriginal proportion of the original tokenA that want to me removed
+     * @param amountOfBOriginal proportion of the original tokenB that want to me removed
+     */
     function _removeLiquidity(uint256 amountOfAOriginal, uint256 amountOfBOriginal) internal {
         (uint256 userTokenABalance, uint256 userTokenBBalance) = _getUserBalances(msg.sender);
         require(
@@ -292,13 +353,11 @@ abstract contract AMM {
         return fImpOpening;
     }
 
-    // function _buyExactTokensWithTokens(uint256 maxAmountOfTokensA, uint256 amountOfTokensB) internal virtual;
-
     function getPoolBalances() public view returns (uint256, uint256) {
         return _getPoolBalances();
     }
 
-    function getWorthUserBalances(address user) public view returns (uint256, uint256) {
+    function getWorthUserBalances(address user) external view returns (uint256, uint256) {
         (uint256 totalTokenA, uint256 totalTokenB) = _getPoolBalances();
         uint256 ABPrice = _getABPrice();
         uint256 fImpOpening = _getFImpOpening(
@@ -313,7 +372,7 @@ abstract contract AMM {
         return (worthBalanceTokenA, worthBalanceTokenB);
     }
 
-    function getMaxWithdrawBalances(address user) public view returns (uint256, uint256) {
+    function getMaxWithdrawBalances(address user) external view returns (uint256, uint256) {
         (uint256 totalTokenA, uint256 totalTokenB) = _getPoolBalances();
         uint256 ABPrice = _getABPrice();
         uint256 fImpOpening = _getFImpOpening(
