a42107c212a619c201451219d5c059054ce95333
Guilherme Viana <ggviana@hotmail.com.br>
2020-10-30 20:00:22 -0300

    Default ownership and automatic strike

--- tasks/option/mintOptions.js
+++ tasks/option/mintOptions.js
@@ -1,20 +1,23 @@
 
 internalTask('mintOptions', 'Mint options')
-  .addOptionalParam('contract', 'Option Contract type to use')
   .addParam('option', 'Option address')
-  .addParam('strike', 'Strike Asset Address')
   .addParam('amount', 'Amount of Options to mint')
-  .addParam('owner', 'Option owner')
-  .setAction(async ({ option, strike, owner, amount, contract = 'PodPut' }) => {
-    const strikeAssetContract = await ethers.getContractAt('MockERC20', strike)
-    const OptionContract = await ethers.getContractAt(contract, option)
+  .addOptionalParam('contract', 'Option Contract type to use')
+  .addOptionalParam('owner', 'Option owner')
+  .setAction(async ({ option, owner, amount, contract = 'PodPut' }, bre) => {
+    if(!owner) {
+      const [defaultOwner] = await bre.web3.eth.getAccounts()
+      owner = defaultOwner
+    }
+    const optionContract = await ethers.getContractAt(contract, option)
+    const strikeAssetContract = await ethers.getContractAt('MockERC20', await optionContract.strikeAsset())
 
     console.log('Strike Asset', await strikeAssetContract.symbol())
     // 1) Approve StrikeAsset between me and option Contract
     await strikeAssetContract.approve(option, (ethers.constants.MaxUint256).toString())
 
     // 2) Call option Mint
-    const txIdMint = await OptionContract.mint(amount, owner)
+    const txIdMint = await optionContract.mint(amount, owner)
     await txIdMint.wait()
-    console.log('Option Balance after mint', (await OptionContract.balanceOf(owner)).toString())
+    console.log('Option Balance after mint', (await optionContract.balanceOf(owner)).toString())
   })
