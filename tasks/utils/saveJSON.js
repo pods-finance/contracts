33a90d664eb8f47d0e1a4b67e505fe02174ea118
Guilherme Viana <ggviana@hotmail.com.br>
2020-11-05 19:46:36 -0300

    Updated to save more values at a time

--- tasks/utils/saveJSON.js
+++ tasks/utils/saveJSON.js
@@ -5,15 +5,14 @@ const util = require('util')
 const writeFile = util.promisify(fs.writeFile)
 const readFile = util.promisify(fs.readFile)
 
-module.exports = async function saveJSON (filePath, key, value) {
+module.exports = async function saveJSON (filePath, values) {
   try {
     const _filePath = path.resolve(filePath)
     const content = await readFile(_filePath)
-    const json = JSON.parse(content)
+    const current = JSON.parse(content)
+    const updated = Object.assign(current, values)
 
-    json[key] = value
-
-    await writeFile(_filePath, JSON.stringify(json, null, 2))
+    await writeFile(_filePath, JSON.stringify(updated, null, 2))
   } catch (err) {
     console.error(err)
     process.exit(1)
