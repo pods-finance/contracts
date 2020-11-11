7fb8e6e0969cdc59fd9caa4c6e69027abe86ea20
Robsonsjre <robsonsjre@gmail.com>
2020-11-11 01:02:36 -0300

    fixed AMMPool interface

--- contracts/interfaces/IOptionAMMPool.sol
+++ contracts/interfaces/IOptionAMMPool.sol
@@ -3,22 +3,82 @@ pragma solidity ^0.6.8;
 
 interface IOptionAMMPool {
     function addLiquidity(
-        uint256 amountOfStable,
-        uint256 amountOfOptions,
-        address recipient
+        uint256 amountOfA,
+        uint256 amountOfB,
+        address owner
     ) external;
 
-    function removeLiquidity(uint256 amountOfStable, uint256 amountOfOptions) external;
+    function removeLiquidity(uint256 amountOfA, uint256 amountOfB) external;
 
-    function buyExact(
-        uint256 maxPayedStable,
-        uint256 amount,
+    function tradeExactAInput(
+        uint256 exactAmountAIn,
+        uint256 minAmountBOut,
+        address owner,
         uint256 sigmaInitialGuess
-    ) external;
+    ) external returns (uint256);
 
-    function buyExactInput(
-        uint256 amount,
-        uint256 minOptionBought,
+    function tradeExactAOutput(
+        uint256 exactAmountAOut,
+        uint256 maxAmountBIn,
+        address owner,
         uint256 sigmaInitialGuess
-    ) external;
+    ) external returns (uint256);
+
+    function tradeExactBInput(
+        uint256 exactAmountBIn,
+        uint256 minAmountAOut,
+        address owner,
+        uint256 sigmaInitialGuess
+    ) external returns (uint256);
+
+    function tradeExactBOutput(
+        uint256 exactAmountBOut,
+        uint256 maxAmountAIn,
+        address owner,
+        uint256 sigmaInitialGuess
+    ) external returns (uint256);
+
+    function getOptionTradeDetailsExactAInput(uint256 exactAmountAIn)
+        external
+        view
+        returns (
+            uint256 amountBOutput,
+            uint256 newSigma,
+            uint256 feesTokenA,
+            uint256 feesTokenB
+        );
+
+    function getOptionTradeDetailsExactAOutput(uint256 exactAmountAOut)
+        external
+        view
+        returns (
+            uint256 amountBInput,
+            uint256 newSigma,
+            uint256 feesTokenA,
+            uint256 feesTokenB
+        );
+
+    function getOptionTradeDetailsExactBInput(uint256 exactAmountBIn)
+        external
+        view
+        returns (
+            uint256 amountAOutput,
+            uint256 newSigma,
+            uint256 feesTokenA,
+            uint256 feesTokenB
+        );
+
+    function getOptionTradeDetailsExactBOutput(uint256 exactAmountBOut)
+        external
+        view
+        returns (
+            uint256 amountAInput,
+            uint256 newSigma,
+            uint256 feesTokenA,
+            uint256 feesTokenB
+        );
+
+    function getSpotPrice(address asset, uint256 decimalsOutput) external view returns (uint256);
+
+    function getABPrice() external view returns (uint256);
 }
