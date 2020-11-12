75b035bc443b978c88b57522ad0982c39d7acb31
Guilherme Viana <ggviana@hotmail.com.br>
2020-11-12 18:43:32 -0300

    Using address on cli for dynamic usage

--- tasks/deployOptionExchange.js
+++ tasks/deployOptionExchange.js
@@ -1,13 +1,12 @@
 
 internalTask('deployOptionExchange', 'Deploy new option exchange using provider')
   .addParam('provider', 'String representing provider name (E.g: UniswapV1Provider)')
-  .addParam('factory', 'String of the factory name to pass to initialize')
+  .addParam('factory', 'Address of the factory to pass to initialize')
   .setAction(async ({ provider, factory }, bre) => {
-    const factoryAddress = require(`../deployments/${bre.network.name}.json`)[factory]
     console.log(`----Start Deploy ${provider}----`)
     const ExchangeProvider = await ethers.getContractFactory(provider)
     // 1) Deploy provider
-    const exchangeProvider = await ExchangeProvider.deploy(factoryAddress)
+    const exchangeProvider = await ExchangeProvider.deploy(factory)
      console.log('Option Provider Address: ', exchangeProvider.address)
     // 2) Deploy Option Exchange
     console.log('----Start Deploy OptionExchange----')
