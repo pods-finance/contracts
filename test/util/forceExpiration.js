7eb5ef17b992bc7e6ac8bcb364f5b0fa6ab07a99
Rob <robsonsjre@gmail.com>
2020-11-16 14:56:57 -0300

    removed unused variable on on forceExpiration
    Co-authored-by: Guilherme Guimarães Viana <ggviana@hotmail.com.br>

--- test/util/forceExpiration.js
+++ test/util/forceExpiration.js
@@ -2,7 +2,6 @@ module.exports = async function forceExpiration (option, number) {
   let expirableTimestamp = number
   if (!number) {
     expirableTimestamp = (await option.expiration()).toNumber()
-    const expirableTimestampString = (await option.expiration()).toString()
   }
   await ethers.provider.send('evm_mine', [expirableTimestamp])
 }
