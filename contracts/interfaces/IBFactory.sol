e79aba66179c44714b959e45aa1b93c36a4e8c66
Robsonsjre <robsonsjre@gmail.com>
2020-12-07 19:40:54 -0300

    remved unused interfaces

--- contracts/interfaces/IBFactory.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.6.12;
-
-interface IBFactory {
-    function newBPool() external returns (address);
-
-    function isBPool(address b) external view returns (bool);
-}
