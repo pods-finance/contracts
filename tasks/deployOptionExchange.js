cc20f0422977149a93a76e31a7012973649c5076
Robsonsjre <robsonsjre@gmail.com>
2020-12-16 08:59:27 -0300

    saving optionExchange to json

--- tasks/deployOptionExchange.js
+++ tasks/deployOptionExchange.js
@@ -1,10 +1,14 @@
+const saveJSON = require('./utils/saveJSON')
 
 internalTask('deployOptionExchange', 'Deploy new option exchange using provider')
   .addParam('factory', 'Address of the factory to pass to initialize')
   .setAction(async ({ factory }, bre) => {
     console.log('----Start Deploy OptionExchange----')
+    const path = `../../deployments/${bre.network.name}.json`
     const OptionExchangeContract = await ethers.getContractFactory('OptionExchange')
     const optionExchange = await OptionExchangeContract.deploy(factory)
     console.log('Option Exchange Address: ', optionExchange.address)
+
+    await saveJSON(path, { optionExchange: optionExchange.address })
     return optionExchange.address
   })
