22b0f0691726e2ff30d24cbb9885f1d32536e4e4
Guilherme Viana <ggviana@hotmail.com.br>
2021-01-08 16:25:07 -0300

    No need for public functions here

--- contracts/configuration/EmergencyStop.sol
+++ contracts/configuration/EmergencyStop.sol
@@ -20,7 +20,7 @@ contract EmergencyStop is IEmergencyStop, Ownable {
      * @dev Checks if a contract should be considered as stopped
      * @param target The contract address
      */
-    function isStopped(address target) public override view returns (bool) {
+    function isStopped(address target) external override view returns (bool) {
         return _addresses[target];
     }
 
@@ -28,7 +28,7 @@ contract EmergencyStop is IEmergencyStop, Ownable {
      * @dev Signals that the target should now be considered as stopped
      * @param target The contract address
      */
-    function stop(address target) public override onlyOwner {
+    function stop(address target) external override onlyOwner {
         _addresses[target] = true;
         emit Stopped(target);
     }
@@ -37,7 +37,7 @@ contract EmergencyStop is IEmergencyStop, Ownable {
      * @dev Signals that the target should now be considered as fully functional
      * @param target The contract address
      */
-    function resume(address target) public override onlyOwner {
+    function resume(address target) external override onlyOwner {
         require(_addresses[target], "EmergencyStop: target is not stopped");
         _addresses[target] = false;
         emit Resumed(target);
