4a362ef6fc51f7561345bfc226343fc99cfbd002
Rob <robsonsjre@gmail.com>
2021-03-30 16:24:07 -0300

    added new test suite for configuration manager (#201)

---
+++ test/configuration/ConfigurationManager.test.js
@@ -0,0 +1,48 @@
+const { ethers } = require('hardhat')
+const { expect } = require('chai')
+
+describe('ConfigurationManager', () => {
+  let ConfigurationManager, configurationManager
+
+  beforeEach(async () => {
+    ConfigurationManager = await ethers.getContractFactory('ConfigurationManager')
+    configurationManager = await ConfigurationManager.deploy()
+  })
+
+  it('sets all modules and get the address correctly', async () => {
+    const randomAddress = '0x0000000000000000000000000000000000000001'
+
+    await configurationManager.setEmergencyStop(randomAddress)
+    await configurationManager.setPricingMethod(randomAddress)
+    await configurationManager.setSigmaGuesser(randomAddress)
+    await configurationManager.setPriceProvider(randomAddress)
+    await configurationManager.setCapProvider(randomAddress)
+    await configurationManager.setAMMFactory(randomAddress)
+    await configurationManager.setOptionFactory(randomAddress)
+    await configurationManager.setOptionHelper(randomAddress)
+
+    expect(await configurationManager.getEmergencyStop())
+      .to.equal(randomAddress)
+
+    expect(await configurationManager.getPricingMethod())
+      .to.equal(randomAddress)
+
+    expect(await configurationManager.getSigmaGuesser())
+      .to.equal(randomAddress)
+
+    expect(await configurationManager.getPriceProvider())
+      .to.equal(randomAddress)
+
+    expect(await configurationManager.getCapProvider())
+      .to.equal(randomAddress)
+
+    expect(await configurationManager.getAMMFactory())
+      .to.equal(randomAddress)
+
+    expect(await configurationManager.getOptionFactory())
+      .to.equal(randomAddress)
+
+    expect(await configurationManager.getOptionHelper())
+      .to.equal(randomAddress)
+  })
+})
