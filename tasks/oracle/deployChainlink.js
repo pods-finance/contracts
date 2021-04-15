0191c96385912f1de1f8ec98617b7ac1f04c3e31
Rob <robsonsjre@gmail.com>
2021-04-15 18:20:03 -0300

    added scripts adaptations (#209)

--- tasks/oracle/deployChainlink.js
+++ tasks/oracle/deployChainlink.js
@@ -1,12 +1,13 @@
-
 task('deployChainlink', 'Deploy Chainlink Contract')
   .addParam('source', 'address of chainlink pricefeed')
   .setAction(async ({ source }) => {
+    const numberOfConfirmations = hre.network.name === 'local' ? 1 : 2
+
     console.log('----Start Deploy Chainlink----')
     const Chainlink = await ethers.getContractFactory('ChainlinkPriceFeed')
     const chainlink = await Chainlink.deploy(source)
+    await chainlink.deployTransaction.wait(numberOfConfirmations)
 
-    await chainlink.deployed(2)
     console.log('Chainlink', chainlink.address)
     return chainlink.address
   })
