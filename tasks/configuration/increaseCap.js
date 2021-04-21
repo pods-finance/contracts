daffcc9fb7464148fa123e98f54ebd0c2c7a4193
Rob <robsonsjre@gmail.com>
2021-04-21 12:39:38 -0300

    added new increase cap task (#215)
    * added new increase cap task
    
    * Update tasks/configuration/increaseCap.js
    
    Co-authored-by: Guilherme Guimarães Viana <ggviana@hotmail.com.br>
    
    * fixed max supply
    
    Co-authored-by: Guilherme Guimarães Viana <ggviana@hotmail.com.br>

---
+++ tasks/configuration/increaseCap.js
@@ -0,0 +1,28 @@
+task('increaseCap', 'Increase an option or pool cap')
+  .addParam('contract', 'An address of a target address to increase cap (either option or pool')
+  .addOptionalParam('value', 'new cap number')
+  .addOptionalParam('configurator', 'An address of a deployed ConfigurationManager, defaults to current `deployments` json file')
+  .addFlag('max', 'set pool cap to the max')
+  .setAction(async ({ contract, value, configurator, max }, bre) => {
+    console.log('======== START MODIFY CONTRACT CAP ==========')
+    const filePath = `../../deployments/${bre.network.name}.json`
+
+    if (!configurator) {
+      const json = require(filePath)
+      configurator = json.configurationManager
+    }
+
+    if (!ethers.utils.isAddress(contract)) {
+      throw new Error(`\`address\` is not an address. Received: ${configurator}`)
+    }
+
+    const configurationManager = await ethers.getContractAt('ConfigurationManager', configurator)
+    const capProviderAddress = await configurationManager.getCapProvider()
+    const capContract = await ethers.getContractAt('CapProvider', capProviderAddress)
+    const valueToSend = max ? 0 : value
+
+    const tx = await capContract.setCap(contract, valueToSend)
+    const txReceipt = tx.wait()
+    console.log(`transactionHash: ${txReceipt.transactionHash}`)
+    console.log('======== END MODIFY CONTRACT CAP ==========')
+  })
