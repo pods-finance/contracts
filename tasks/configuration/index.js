406e5d293ca715ebf0f06201e93298f19a608474
Guilherme Guimarães Viana <ggviana@hotmail.com.br>
2021-05-11 13:34:33 -0300

    Parameter scripts (#242)
    * Added parameter scripts
    
    * Fixed script
    
    Co-authored-by: Rob <robsonsjre@gmail.com>

--- tasks/configuration/index.js
+++ tasks/configuration/index.js
@@ -1,5 +1,7 @@
 require('./deployConfigurationManager')
 require('./linkConfigurationManager')
 require('./inspectConfigurationManager')
+require('./getParameters')
 require('./emergencyStop')
 require('./increaseCap')
+require('./setParameter')
