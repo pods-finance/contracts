f6a116e4537fe58b2c7d86f246c1a776ae5168e3
Robsonsjre <robsonsjre@gmail.com>
2021-05-21 04:44:36 -0300

    adding and fixing deployment scripts

--- tasks/setupFullNetwork.js
+++ tasks/setupFullNetwork.js
@@ -5,12 +5,14 @@ task('setupFullNetwork', 'Deploy a whole local Kovan environment')
   .addFlag('verify', 'bool if the contract should be verified or not')
   .setAction(async ({ asset, source, verify, wethadapt }, hre) => {
     await hre.run('compile')
-    const cnotracts = require(`../deployments/${hre.network.name}.json`)
+    const contracts = require(`../deployments/${hre.network.name}.json`)
 
     const assetUpper = asset.toUpperCase()
-    const assetAddress = cnotracts[assetUpper]
+    const assetAddress = contracts[assetUpper]
     // Erasing local.json file
     const configurationManagerAddress = await run('deployConfigurationManager', { verify })
 
     await run('setAMMEnvironment', { asset: assetAddress, source: source, configuration: configurationManagerAddress, builders: true, wethadapt, verify })
   })
+
+// npx hardhat setupFullNetwork --network kovan --asset weth --source 0x9326BFA02ADD2366b30bacB125260Af641031331--verify
--- tasks/utils/deploy.js
+++ tasks/utils/deploy.js
@@ -1,23 +1,29 @@
 const verifyContract = require('../utils/verify')
+const saveJSON = require('../utils/saveJSON')
 
 task('deploy', 'Deploy a generic contract given artifact name')
   .addParam('name', 'name of the contract artifact')
   .addFlag('verify', 'if true, it should verify the contract after the deployment')
-  .setAction(async ({ name, verify }) => {
+  .addFlag('save', 'if true, it should save the contract address inside the deployments folder')
+  .setAction(async ({ name, verify, save }) => {
     console.log('----Start Deploy Contract----')
     const numberOfConfirmations = hre.network.name === 'local' ? 1 : 2
     const Contract = await ethers.getContractFactory(name)
     const contract = await Contract.deploy()
     await contract.deployTransaction.wait(numberOfConfirmations)
 
-    if (verify) {
-      hre.run('verify:verify', { contract: name, address: contract.address })
-    }
-
     console.log(`${name} Address: ${contract.address}`)
 
-    if(verify) {
-        await verifyContract(hre, contract.address)
+    if (verify) {
+      await verifyContract(hre, contract.address)
+    }
+
+    if (save) {
+      const saveObj = {
+        [name]: contract.address
+      }
+
+      await saveJSON(`../../deployments/${hre.network.name}.json`, saveObj)
     }
 
     return contract.address
