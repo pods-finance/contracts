a4ae5bf6456d8859ca05d10a2f20afe032ed9193
Robsonsjre <robsonsjre@gmail.com>
2021-05-21 03:06:28 -0300

    removed console

--- contracts/amm/FeePool.sol
+++ contracts/amm/FeePool.sol
@@ -8,7 +8,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "../interfaces/IFeePool.sol";
-import "hardhat/console.sol";
 
 /**
  * @title FeePool
