b7b58141001722db0b19fb54983a5cdb4f3bd57e
Guilherme Viana <ggviana@hotmail.com.br>
2021-05-31 00:25:28 -0300

    Fixed local script

--- tasks/local/setupLocal.js
+++ tasks/local/setupLocal.js
@@ -5,65 +5,65 @@ const pathJoin = require('path')
 const fsPromises = fs.promises
 
 task('setupLocal', 'Deploy a whole local test environment')
-  .setAction(async ({}, bre) => {
-    const path = `../../deployments/${bre.network.name}.json`
+  .setAction(async ({}, hre) => {
+    const path = `../../deployments/${hre.network.name}.json`
     // Erasing local.json file
     await saveJSON(path, '', true)
 
     // 1) Setup mock assets
-    const mockWETH = await run('deployToken', { name: 'weth', symbol: 'weth', decimals: '18', weth: true })
+    const mockWETHAddress = await hre.run('deployToken', { name: 'Wrapped Ether', symbol: 'WETH', decimals: '18', weth: true })
 
-    const mockUSDC = await run('deployToken', { name: 'USDC Token', symbol: 'USDC', decimals: '6' })
+    const mockUSDCAddress = await hre.run('deployToken', { name: 'USD Coin', symbol: 'USDC', decimals: '6' })
 
-    const mockAUSDC = await run('deployToken', { name: 'AUSDC Token', symbol: 'AUSDC', decimals: '6' })
+    const mockAUSDCAddress = await hre.run('deployToken', { name: 'Aave Interest bearing USDC', symbol: 'aUSDC', decimals: '6' })
 
-    const mockDAI = await run('deployToken', { name: 'DAI Token', symbol: 'DAI', decimals: '18' })
+    const mockDAIAddress = await hre.run('deployToken', { name: 'Dai Stablecoin', symbol: 'DAI', decimals: '18' })
 
-    const mockWBTC = await run('deployToken', { name: 'Wrappeed BTC', symbol: 'WBTC', decimals: '8' })
+    const mockWBTCAddress = await hre.run('deployToken', { name: 'Wrapped BTC', symbol: 'WBTC', decimals: '8' })
 
-    const mockLINK = await run('deployToken', { name: 'LINK Address', symbol: 'LINK', decimals: '18' })
+    const mockLINKAddress = await hre.run('deployToken', { name: 'ChainLink Token', symbol: 'LINK', decimals: '18' })
 
     const tokensObj = {
-      WETH: mockWETH.address,
-      USDC: mockUSDC.address,
-      AUSDC: mockAUSDC.address,
-      DAI: mockDAI.address,
-      WBTC: mockWBTC.address,
-      LINK: mockLINK.address
+      WETH: mockWETHAddress,
+      USDC: mockUSDCAddress,
+      AUSDC: mockAUSDCAddress,
+      DAI: mockDAIAddress,
+      WBTC: mockWBTCAddress,
+      LINK: mockLINKAddress
 
     }
     await saveJSON(path, tokensObj)
 
-    const configurationManagerAddress = await run('deployConfigurationManager')
+    const configurationManagerAddress = await hre.run('deployConfigurationManager')
 
     // 2) Setup Chainlink (Oracle) Mock
     const ChainlinkWBTCFeed = await ethers.getContractFactory('MockChainlinkFeed')
 
-    const chainlinkWBTCFeed = await ChainlinkWBTCFeed.deploy(mockWBTC.address, '8', '37170000000000')
-    const chainlinkWETHFeed = await ChainlinkWBTCFeed.deploy(mockWETH.address, '8', '1270000000000')
-    const chainlinkLINKFeed = await ChainlinkWBTCFeed.deploy(mockLINK.address, '8', '2496201073')
+    const chainlinkWBTCFeed = await ChainlinkWBTCFeed.deploy(mockWBTCAddress, '8', '37170000000000')
+    const chainlinkWETHFeed = await ChainlinkWBTCFeed.deploy(mockWETHAddress, '8', '1270000000000')
+    const chainlinkLINKFeed = await ChainlinkWBTCFeed.deploy(mockLINKAddress, '8', '2496201073')
 
     await saveJSON(path, { wbtcChainlinkFeed: chainlinkWBTCFeed.address })
 
     // 3.2) Deploy BS + IV + AMMPoolFactory + Oracles
-    await run('setAMMEnvironment', { asset: mockWBTC.address, source: chainlinkWBTCFeed.address, configuration: configurationManagerAddress, builders: true })
+    await run('setAMMEnvironment', { asset: mockWBTCAddress, source: chainlinkWBTCFeed.address, configuration: configurationManagerAddress, builders: true })
 
     // 3.3) Deploy Option Exchange
     const _filePath = pathJoin.join(__dirname, path)
     const content = await fsPromises.readFile(_filePath)
 
+    const configurationManager = await ethers.getContractAt('ConfigurationManager', JSON.parse(content).ConfigurationManager)
+
     // Set WETH price Provider
-    const priceProvider = await ethers.getContractAt('PriceProvider', JSON.parse(content).PriceProvider)
+    const priceProvider = await ethers.getContractAt('PriceProvider', await configurationManager.getPriceProvider())
 
-    await priceProvider.setAssetFeeds([mockWETH.address], [chainlinkWETHFeed.address])
-    await priceProvider.setAssetFeeds([mockLINK.address], [chainlinkLINKFeed.address])
-
-    const optionAMMFactory = JSON.parse(content).OptionAMMFactory
+    await priceProvider.setAssetFeeds([mockWETHAddress], [chainlinkWETHFeed.address])
+    await priceProvider.setAssetFeeds([mockLINKAddress], [chainlinkLINKFeed.address])
 
     // 4) Deploy Test Option
     const currentBlockTimestamp = await getTimestamp()
 
-    const optionWBTCAddress = await run('deployNewOption', {
+    const optionWBTCAddress = await hre.run('deployNewOption', {
       strike: 'USDC',
       underlying: 'WBTC',
       price: '18000',
@@ -71,7 +71,7 @@ task('setupLocal', 'Deploy a whole local test environment')
       cap: '2000'
     })
 
-    const optionWETHAddress = await run('deployNewOption', {
+    const optionWETHAddress = await hre.run('deployNewOption', {
       strike: 'USDC',
       underlying: 'WETH',
       price: '1500',
@@ -79,7 +79,7 @@ task('setupLocal', 'Deploy a whole local test environment')
       cap: '2000'
     })
 
-    const optionLINKAddress = await run('deployNewOption', {
+    const optionLINKAddress = await hre.run('deployNewOption', {
       strike: 'USDC',
       underlying: 'LINK',
       price: '25',
@@ -88,36 +88,37 @@ task('setupLocal', 'Deploy a whole local test environment')
     })
 
     // 5) Create AMMPool test with this asset
-    const optionAMMPoolAddress = await run('deployNewOptionAMMPool', {
+    const optionAMMPoolAddress = await hre.run('deployNewOptionAMMPool', {
       option: optionWBTCAddress,
-      tokenb: mockUSDC.address,
+      tokenb: mockUSDCAddress,
       initialiv: '770000000000000000', // 0.77%
       cap: '500000'
     })
 
-    const optionAMMETHPoolAddress = await run('deployNewOptionAMMPool', {
+    const optionAMMETHPoolAddress = await hre.run('deployNewOptionAMMPool', {
       option: optionWETHAddress,
-      tokenb: mockUSDC.address,
+      tokenb: mockUSDCAddress,
       initialiv: '2000000000000000000',
       cap: '500000'
     })
 
-    const optionLINKPoolAddress = await run('deployNewOptionAMMPool', {
+    const optionLINKPoolAddress = await hre.run('deployNewOptionAMMPool', {
       option: optionLINKAddress,
-      tokenb: mockUSDC.address,
+      tokenb: mockUSDCAddress,
       initialiv: '2311200000000000000',
       cap: '500000'
     })
 
     // 6) Mint Strike Asset
     console.log('Minting USDC strike asset')
+    const mockUSDC = await ethers.getContractAt('MintableERC20', mockUSDCAddress)
     await mockUSDC.mint('10000000000000000')
 
-    // 7) Mint Options
-    await run('mintOptions', { option: optionLINKAddress, amount: '600' })
+    // // 7) Mint Options
+    await hre.run('mintOptions', { option: optionLINKAddress, amount: '600' })
 
     // 8) Add Liquidity
-    await run('addLiquidityAMM', {
+    await hre.run('addLiquidityAMM', {
       pooladdress: optionLINKPoolAddress,
       amounta: '500',
       amountb: '11000'
