1ff83af475ff195f1ff358e6c612312ffbdb8ffa
Robsonsjre <robsonsjre@gmail.com>
2021-06-21 23:01:47 -0300

    removed order

--- contracts/configuration/ConfigurationManager.sol
+++ contracts/configuration/ConfigurationManager.sol
@@ -11,7 +11,7 @@ import "../interfaces/IConfigurationManager.sol";
  * @author Pods Finance
  * @notice Allows contracts to read protocol-wide configuration modules
  */
-contract ConfigurationManager is Ownable, IConfigurationManager, ModuleStorage {
+contract ConfigurationManager is IConfigurationManager, ModuleStorage, Ownable {
     mapping(bytes32 => uint256) private _parameters;
 
     /* solhint-disable private-vars-leading-underscore */
