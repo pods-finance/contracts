5086c804bbd652115389ecb1b3620870a45302a6
Guilherme Viana <ggviana@hotmail.com.br>
2021-07-08 22:55:00 -0500

    Changed deploy bs to task

--- tasks/amm/deployBS.js
+++ tasks/amm/deployBS.js
@@ -1,6 +1,6 @@
 const validateAddress = require('../utils/validateAddress')
 
-internalTask('deployBS', 'Deploy Black Scholes')
+task('deployBS', 'Deploy Black Scholes')
   .addFlag('verify', 'if true, it should verify the contract after the deployment')
   .addParam('normaldist', 'Normal Distribution Address')
   .setAction(async ({ normaldist, verify }, hre) => {
