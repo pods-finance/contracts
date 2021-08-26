202047a878d280724b4aa7202d7cba0dd0099660
Guilherme Viana <ggviana@hotmail.com.br>
2021-08-26 00:30:04 -0300

    Added verify and common option cases

--- tasks/oracle/deployChainlink.js
+++ tasks/oracle/deployChainlink.js
@@ -4,13 +4,14 @@ task('deployChainlink', 'Deploy Chainlink Contract')
   .addParam('source', 'address of chainlink pricefeed')
   .addFlag('save', 'if true, it should save the contract address inside the deployments folder')
   .addFlag('verify', 'if true, it should verify the contract after the deployment')
-  .setAction(async ({ source, save }, hre) => {
+  .setAction(async ({ source, save, verify }, hre) => {
     validateAddress(source, 'source')
 
     const address = await hre.run('deploy', {
       name: 'ChainlinkPriceFeed',
       args: [source],
-      save
+      save,
+      verify
     })
 
     return address
